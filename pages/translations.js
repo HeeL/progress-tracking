8c8345fedb01329b165b3d50f8eee07d252a08cb
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2019-08-03 21:28:08 +0200

    Return stubbed data in translations for now

--- pages/translations.js
+++ pages/translations.js
@@ -1,1 +1,19 @@
-export default () => <div>Translations</div>;
+import React from "react";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+
+const Index = props => (
+  <div>
+    {(props.total - props.remain) * 100 / props.total }%
+  </div>
+);
+
+Index.getInitialProps = () => {
+    return {
+        remain: 50,
+        total: 100
+    };
+    fetch('https://www.holidaycheck.de/api/translations')
+        .then(result => result.json());
+}
+export default Index;
