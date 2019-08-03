1bb22148de19f0a8b825c38c268345649603949f
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2019-08-03 22:36:49 +0200

    Show progressbar and add styling

--- pages/translations.js
+++ pages/translations.js
@@ -1,19 +1,23 @@
 import React from "react";
-import Link from "next/link";
 import fetch from "isomorphic-unfetch";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
-const Index = props => (
-  <div>
-    {(props.total - props.remain) * 100 / props.total }%
+const Index = ({ percentage }) => (
+  <div style={{width: '85vh', textAlign: 'center', margin: 'auto'}}>
+        <h1>Removed Translations</h1>
+        <CircularProgressbar value={percentage} text={`${percentage}%`} />
   </div>
 );
 
 Index.getInitialProps = () => {
+    const total = 500;
+    const remain = 250;
+    const percentage = (total - remain) * 100 / total;
     return {
-        remain: 50,
-        total: 100
+        percentage
     };
-    fetch('https://www.holidaycheck.de/api/translations')
-        .then(result => result.json());
+    // replace stubbed data with this api call
+    // fetch('https://www.holidaycheck.de/api/translations').then(result => result.json());
 }
 export default Index;
